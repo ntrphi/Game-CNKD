8b0958d74807202b23d0770e16da51057f557031
phiphi <nguyentruongphi155@gmailcom>
2019-03-18 23:21:35 +0700

    Initial commit

---
+++ public/js/question.js
@@ -0,0 +1,26 @@
+var questions = [
+    {
+        q: "Thủ đô của nước Anh",
+        a: "Manchester"
+    },
+    {
+        q: "Worldcup 2050 được tổ chức ở đâu",
+        a: "VietNam"
+    },
+    {
+        q: "Sông nào dài nhất Việt Nam?",
+        a: "songcuulong"
+    },
+    {
+        q: "Núi cao nhất ở Việt Nam tên là gì?",
+        a: "fanxipang"
+    },
+    {
+        q: "Tỉnh nào rộng nhất Việt Nam?",
+        a: "hanoi"
+    }
+];
+var bangChuCai = [
+    "a", "b", 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','s', 't','u','v','w','x','y','z'
+]
+
